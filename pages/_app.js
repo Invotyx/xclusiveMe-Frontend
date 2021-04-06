73e3075cb9a3d57ca6f881470ef7b6cdb61d1c2c
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 12:25:34 +0500

    Revert "globals removed"
    This reverts commit 65aa4a10fb2be924fde355eef0df7560e8d46601.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { darkTheme } from '../theme';
