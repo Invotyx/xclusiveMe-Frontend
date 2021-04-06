393aebd8c902f93c7ffe84fb1fc64c3d29fb5ad3
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 11:47:20 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,22 @@ import { darkTheme } from '../theme';
 import Image from 'next/image';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#000',
+  },
+}));
 
 function MyApp({ Component, pageProps }) {
+  const classes = useStyles();
+
   return (
     <>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <Grid container component='main'>
+        <Grid container component='main' className={classes.root}>
           <Grid item xs={12}>
             <Box p={8}>
               <Image width={50} height={50} src='/logo.png' alt='Logo' />
