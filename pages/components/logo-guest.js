c10b5a6ea0c4f82f103f6eeebb6cfe27fa30e3bc
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 14:26:46 +0500

    Update logo-guest.js

--- pages/components/logo-guest.js
+++ pages/components/logo-guest.js
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Box from '@material-ui/core/Box';
 
 export default function Comp() {
   return (
     <Box p={8}>
-      <Image width={50} height={50} src='/logo.png' alt='Logo' />
+      <Link href='/'>
+        <Image width={50} height={50} src='/logo.png' alt='Logo' />
+      </Link>
     </Box>
   );
 }
