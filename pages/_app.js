4651cd54ed9a1d806c7733de86d713a3467bcdfa
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 14:20:05 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { darkTheme } from '../theme';
 import { makeStyles } from '@material-ui/core/styles';
+import Head from 'next/head';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,6 +23,13 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>xclusiveme</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Component {...pageProps} />
