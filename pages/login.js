c29bb57b58c1b0a4a4debd75d4d53d16061a95ed
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 12:34:51 +0500

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -116,6 +116,7 @@ export default function SignInSide() {
                 fullWidth
                 variant='contained'
                 color='primary'
+                href='/dashboard'
               >
                 Login
               </TileButton>
