d258aeb4f5b29571ad3c3d2b5b20eb17f8587241
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 11:28:19 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
 export default function Home() {
   return (
@@ -17,44 +20,28 @@ export default function Home() {
         />
       </Head>
 
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <Grid item xs={12} sm={8} md={5}>
+        <Box p={6} mx={4}>
+          <Box py={4} display='block'>
+            <Typography component='h1' variant='h4'>
+              Exclusive club for influencers and members to share exclusive
+              content.
+            </Typography>
+          </Box>
+          <Box mr={2} mt={2} display='flex'>
+            <Box mr={2}>
+              <Button variant='outlined' href='/login'>
+                Login
+              </Button>
+            </Box>
+            <Box mr={2}>
+              <Button variant='outlined' href='/login'>
+                Create Account
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Grid>
     </>
   );
 }
