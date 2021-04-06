a54b89bc7588408b7773b2f0bfced42b7b157ef9
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 11:14:40 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { darkTheme } from '../theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+          <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
