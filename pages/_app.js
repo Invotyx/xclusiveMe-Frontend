4c0d2b33bd455df2849947e0e1596f8b8cbcd8cd
Salal Aslam <salalaslam@yahoo.com>
2021-04-06 11:14:51 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { darkTheme } from '../theme';
+import Image from 'next/image';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
+        <Grid container component='main'>
+          <Grid item xs={12}>
+            <Box p={8}>
+              <Image width={50} height={50} src='/logo.png' alt='Logo' />
+            </Box>
+          </Grid>
           <Component {...pageProps} />
+        </Grid>
       </ThemeProvider>
     </>
   );
