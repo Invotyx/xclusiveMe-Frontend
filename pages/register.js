b04210f2b1055320e4d5fa348a605c9bf52c6fa6
Salal Aslam <salalaslam@yahoo.com>
2021-04-07 16:26:28 +0500

    Create register.js

---
+++ pages/register.js
@@ -0,0 +1,144 @@
+import { useRouter } from 'next/router';
+import axios from '../axios';
+import { useState } from 'react';
+import Head from 'next/head';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import AuthNav from './components/auth-nav';
+import { makeStyles } from '@material-ui/core/styles';
+import HomeLegend from './components/home-legend';
+import TileButton from './components/TileButton';
+import TileTextField from './components/TileTextField';
+import LogoGuest from './components/logo-guest';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  grey: {
+    color: '#666',
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+  const router = useRouter();
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('users', { fullName, username, email, password, phoneNumber })
+      .then((data) => {
+        localStorage.setItem('jwtToken', data.accessToken);
+        router.push('/explore');
+      });
+  };
+
+  return (
+    <Container>
+      <Head>
+        <title>Register</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+        />
+      </Head>
+      <Grid container component='main'>
+        <Grid item xs={12}>
+          <LogoGuest />
+        </Grid>
+        <Grid item xs={12} sm={8} md={5}>
+          <Box p={8} mx={4}>
+            <AuthNav classes={classes} />
+            <form onSubmit={handleSubmit}>
+              <TileTextField
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                variant='outlined'
+                margin='normal'
+                required
+                fullWidth
+                id='fullName'
+                label='Full Name'
+                name='fullName'
+                autoComplete='fullName'
+              />
+              <TileTextField
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                variant='outlined'
+                margin='normal'
+                required
+                fullWidth
+                id='username'
+                label='Username'
+                name='username'
+                autoComplete='username'
+              />
+              <TileTextField
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                variant='outlined'
+                margin='normal'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+                autoFocus
+              />
+              <TileTextField
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                variant='outlined'
+                margin='normal'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='current-password'
+              />
+              <TileTextField
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                variant='outlined'
+                margin='normal'
+                required
+                fullWidth
+                id='phoneNumber'
+                label='Phone Number'
+                name='phoneNumber'
+                autoComplete='phoneNumber'
+                autoFocus
+              />
+              <Box my={2}>
+                <TileButton
+                  type='submit'
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+                >
+                  Register
+                </TileButton>
+              </Box>
+            </form>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4} md={7}>
+          <HomeLegend />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
