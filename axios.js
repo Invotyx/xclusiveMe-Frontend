20725e3892131e7a83967551fb1af593570080f6
Salal Aslam <salalaslam@yahoo.com>
2021-04-07 16:51:05 +0500

    Update axios.js

--- axios.js
+++ axios.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
+const headers = localStorage.getItem('jwtToken')
+  ? { Authorization: 'Bearer ' + localStorage.getItem('jwtToken') }
+  : null;
 const instance = axios.create({
   baseURL: publicRuntimeConfig.backendUrl,
+  headers,
 });
 
 export default instance;
