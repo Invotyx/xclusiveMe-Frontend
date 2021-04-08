8255bf35a58a48146bcd7a41ff3568b7f9a2c020
Salal Aslam <salalaslam@yahoo.com>
2021-04-08 12:13:15 +0500

    Revert "Update axios.js"
    This reverts commit 20725e3892131e7a83967551fb1af593570080f6.

--- axios.js
+++ axios.js
@@ -2,12 +2,8 @@ import axios from 'axios';
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-const headers = localStorage.getItem('jwtToken')
-  ? { Authorization: 'Bearer ' + localStorage.getItem('jwtToken') }
-  : null;
 const instance = axios.create({
   baseURL: publicRuntimeConfig.backendUrl,
-  headers,
 });
 
 export default instance;
