daa99930496117437ddc6fe935ad469ba0a943eb
Salal Aslam <salalaslam@yahoo.com>
2021-04-15 15:54:48 +0500

    Update post.js

--- components/post.js
+++ components/post.js
@@ -17,7 +17,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: '150px',
+    marginTop: 0,
   },
   media: {
     height: 0,
