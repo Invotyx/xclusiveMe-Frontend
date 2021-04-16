910dff426c1759a5d8d3d02210a7e8a66f6a714b
Salal Aslam <salalaslam@yahoo.com>
2021-04-16 09:59:04 +0500

    Update post.js

--- components/post.js
+++ components/post.js
@@ -18,6 +18,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: 0,
+    backgroundColor: 'transparent',
   },
   media: {
     height: 0,
