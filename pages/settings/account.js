bc25cc767f959b6101051d1caac0f44f87fba8dc
Salal Aslam <salalaslam@yahoo.com>
2021-04-19 10:59:35 +0500

    Update account.js

--- pages/settings/account.js
+++ pages/settings/account.js
@@ -112,16 +112,7 @@ export default function Home(props) {
   return (
     <Layout sidebarMenu={sidebarMenu} set_sidebarMenu={set_sidebarMenu}>
       <Head>
-        <title>xclusiveme</title>
-        <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width'
-        />
-        <link rel='icon' href='/favicon.ico' />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-        />
+        <title>Account — Settings</title>
       </Head>
       <Container maxWidth='md'>
         <Grid container spacing={6} className={classes.root}>
