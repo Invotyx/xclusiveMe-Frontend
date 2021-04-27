ccad9738ca7bd033eb4081073f91c73ed2a879ca
Salal Aslam <salalaslam@yahoo.com>
2021-04-27 11:04:16 +0500

    Update index.js

--- sagas/index.js
+++ sagas/index.js
@@ -4,5 +4,9 @@ import authSagas from './authSagas';
 import paymentSagas from './payment.saga';
 
 export default function* rootSaga() {
-  yield all([fork(authSagas), fork(employeeSagas), fork(paymentSagas)]);
+  yield all([
+    fork(authSagas),
+    fork(employeeSagas),
+    fork(paymentSagas),
+  ]);
 }
