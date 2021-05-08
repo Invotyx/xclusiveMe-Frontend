aebee7bd5347a7d7fc490d37695f1c53fc661c2e
honeynaik <51706603+hannannaik@users.noreply.github.com>
2021-05-08 11:38:22 +0500

    Create types.js

---
+++ actions/bottom-alert/types.js
@@ -0,0 +1,9 @@
+import { createActionTypes } from '../../utils';
+
+export const BOTTOMALERT = createActionTypes('BOTTOMALERT', [
+  'UPDATE',
+  'SHOW',
+  'HIDE',
+]);
+
+export default BOTTOMALERT;
