87f8b1018af0616a6a1f4ca8a9363dc427f4feeb
Salal Aslam <salalaslam@yahoo.com>
2021-05-20 11:17:07 +0500

    Create GreenButton.js

---
+++ components/GreenButton.js
@@ -0,0 +1,16 @@
+import NormalCaseButton from './NormalCaseButton';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2ae597', contrastText: '#fff' },
+  },
+});
+
+export default function (props) {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <NormalCaseButton {...props} />
+    </MuiThemeProvider>
+  );
+}
