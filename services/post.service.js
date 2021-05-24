046e77c6a9acd485bf7cd432df2bbdb2f9617031
Salal Aslam <salalaslam@yahoo.com>
2021-05-24 10:21:30 +0500

    Update post.service.js

--- services/post.service.js
+++ services/post.service.js
@@ -3,7 +3,19 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 const SERVER_ADDRESS = publicRuntimeConfig.backendUrl;
 
+export async function getAll() {
+  return apiClient.get(`${SERVER_ADDRESS}/post`);
+}
+
 export async function add(saveData) {
   const data = JSON.stringify(saveData);
   return apiClient.post(`${SERVER_ADDRESS}/post`, data);
+}
+
+export async function update(id, data) {
+  return apiClient.put(`${SERVER_ADDRESS}/post/${id}`, data);
+}
+
+export async function destory(id) {
+  return apiClient.delete(`${SERVER_ADDRESS}/post/${id}/remove`);
 }
