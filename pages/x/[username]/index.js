e9fecdd06960af086dc76c1c952a7676f3d91633
Salal Aslam <salalaslam@yahoo.com>
2021-05-24 14:22:10 +0500

    Update index.js

--- pages/x/[username]/index.js
+++ pages/x/[username]/index.js
@@ -1,9 +1,16 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { user } from '../../../actions/user';
 import User from '../../../components/profile/user';
+import { singleSelector } from '../../../selectors/userSelector';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const router = useRouter();
   const { username } = router.query;
+  useEffect(() => dispatch(user.requestOne(username), [dispatch]));
+  const u = useSelector(singleSelector);
 
-  return <User username={username} />;
+  return <User user={u} />;
 }
