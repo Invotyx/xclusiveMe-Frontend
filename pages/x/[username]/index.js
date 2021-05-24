ce8702a661672fa0668f6d3f6fa4e3dbf9239a12
Salal Aslam <salalaslam@yahoo.com>
2021-05-24 11:05:49 +0500

    Create index.js

---
+++ pages/x/[username]/index.js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+import User from '../../components/profile/user';
+
+export default function Profile() {
+  const router = useRouter();
+  const { username } = router.query;
+
+  return <User username={username} />;
+}
