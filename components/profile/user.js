74c87ab3ca81d99db1d2237ca461b87f32d03fc8
Salal Aslam <salalaslam@yahoo.com>
2021-05-25 09:09:06 +0500

    Update user.js

--- components/profile/user.js
+++ components/profile/user.js
@@ -65,7 +65,7 @@ function TabPanel(props) {
     <div role='tabpanel' hidden={value !== index} {...other}>
       {value === index && (
         <Box p={3}>
-          <Typography>{children}</Typography>
+          <Typography component='div'>{children}</Typography>
         </Box>
       )}
     </div>
