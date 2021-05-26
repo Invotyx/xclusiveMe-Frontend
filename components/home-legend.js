24a7e068cc8bd211e49880c306d43cbf1c6703d6
Salal Aslam <salalaslam@yahoo.com>
2021-05-26 10:21:55 +0500

    Update home-legend.js

--- components/home-legend.js
+++ components/home-legend.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Box from '@material-ui/core/Box';
 
-export default function Comp() {
+export default function HomeLegend() {
   return (
     <Box pt={8} pl={8} pr={8} mt='-50px'>
       <Image width={420} height={420} src='/lead-image.png' alt='Lead Image' />
