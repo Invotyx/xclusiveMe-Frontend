dbf088c3331875cc5b3e06c18e9cc23c897e82b0
Salal Aslam <salalaslam@yahoo.com>
2021-05-26 10:22:01 +0500

    Update logo-guest.js

--- components/layouts/logo-guest.js
+++ components/layouts/logo-guest.js
@@ -1,7 +1,7 @@
 import Box from '@material-ui/core/Box';
 import Logo from './logo';
 
-export default function Comp() {
+export default function LogoGuest() {
   return (
     <Box pt={8} pl={8} pr={8}>
       <Logo />
