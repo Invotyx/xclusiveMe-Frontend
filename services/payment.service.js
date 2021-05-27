5475bde09fb6d8ad04375cf7105a48db44eebab8
Salal Aslam <salalaslam@yahoo.com>
2021-05-27 11:14:40 +0500

    Update payment.service.js

--- services/payment.service.js
+++ services/payment.service.js
@@ -4,7 +4,7 @@ const { publicRuntimeConfig } = getConfig();
 const SERVER_ADDRESS = publicRuntimeConfig.backendUrl;
 
 export async function getUserPaymentMethods() {
-  return apiClient.get(`${SERVER_ADDRESS}/payment-methods`);
+  return apiClient.get(`${SERVER_ADDRESS}/users/payment-methods`);
 }
 
 export async function addPayment(name, token) {
@@ -12,13 +12,17 @@ export async function addPayment(name, token) {
     name: name,
     token: token,
   });
-  return apiClient.post(`${SERVER_ADDRESS}/payment-methods`, data);
+  return apiClient.post(`${SERVER_ADDRESS}/users/payment-methods`, data);
 }
 
 export async function deletePaymentMethod(id) {
-  return apiClient.delete(`${SERVER_ADDRESS}/payment-methods/${id}/remove`);
+  return apiClient.delete(
+    `${SERVER_ADDRESS}/users/payment-methods/${id}/remove`
+  );
 }
 
 export async function setPaymentMethod(id) {
-  return apiClient.post(`${SERVER_ADDRESS}/payment-methods/set-default/${id}`);
+  return apiClient.post(
+    `${SERVER_ADDRESS}/users/payment-methods/set-default/${id}`
+  );
 }
