0921695bab01f92d655d69ec1f021a26fc0e2ca6
Salal Aslam <salalaslam@yahoo.com>
2021-05-27 12:07:01 +0500

    Update index.js

--- pages/x/[username]/index.js
+++ pages/x/[username]/index.js
@@ -1,10 +1,8 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { post } from '../../../actions/post';
 import { user } from '../../../actions/user';
 import User from '../../../components/profile/user';
-// import { xfeedSelector } from '../../../selectors/postSelector';
 import { singleSelector } from '../../../selectors/userSelector';
 
 export default function Profile() {
@@ -13,10 +11,8 @@ export default function Profile() {
   const { username } = router.query;
   useEffect(() => {
     dispatch(user.requestOne(username));
-    // dispatch(post.requestX({ username }));
   }, [username]);
   const u = useSelector(singleSelector);
-  // const _feed = useSelector(xfeedSelector);
 
   return (
     <User
