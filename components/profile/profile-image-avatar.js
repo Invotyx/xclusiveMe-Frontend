f244439a25e02b1f51211f226331b1b798d762c7
Salal Aslam <salalaslam@yahoo.com>
2021-05-28 14:55:40 +0500

    Update profile-image-avatar.js

--- components/profile/profile-image-avatar.js
+++ components/profile/profile-image-avatar.js
@@ -1,5 +1,7 @@
 import ImageAvatar from '../image-avatar';
 
 export default function ProfileImageAvatar({ user, ...props }) {
-  return <ImageAvatar src={user.profileImage} alt={user.fullName} {...props} />;
+  return (
+    <ImageAvatar src={user?.profileImage} alt={user?.fullName} {...props} />
+  );
 }
