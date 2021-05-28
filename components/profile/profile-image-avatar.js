68599e447e4a6e2c2f5300ecd0c4ee37217577dc
Salal Aslam <salalaslam@yahoo.com>
2021-05-28 10:33:28 +0500

    Update profile-image-avatar.js

--- components/profile/profile-image-avatar.js
+++ components/profile/profile-image-avatar.js
@@ -1,7 +1,5 @@
 import ImageAvatar from '../image-avatar';
 
 export default function FormDialog(props) {
-  const { user, ...other } = props;
-
-  return <ImageAvatar {...other} user={user} />;
+  return <ImageAvatar {...props} />;
 }
