f4cfc4390887866582b124f4d03b5248d0f74751
Salal Aslam <salalaslam@yahoo.com>
2021-06-01 12:26:43 +0500

    Update images.js

--- components/profile/images.js
+++ components/profile/images.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import Grid from '@material-ui/core/Grid';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
 
 const tileData = [
   {
@@ -69,15 +70,9 @@ const tileData = [
 ];
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-  },
-  gridList: {
-    width: 500,
-    height: 450,
+  media: {
+    height: 0,
+    paddingTop: '100%',
   },
 }));
 
@@ -85,14 +80,18 @@ export default function ImageGridList() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={3}>
-        {tileData.map((tile) => (
-          <GridListTile key={tile.img} cols={tile.cols || 1}>
-            <img src={tile.img} alt={tile.title} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+    <Grid container spacing={2}>
+      {tileData.map((tile, i) => (
+        <Grid item cols={12} md={4} key={i}>
+          <Card>
+            <CardMedia
+              image={tile.img}
+              title={tile.title}
+              className={classes.media}
+            />
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
