2780070b9b9b619a6ba281f8865f37571807a293
Salal Aslam <salalaslam@yahoo.com>
2021-06-02 15:36:16 +0500

    Update countries.js

--- services/countries.js
+++ services/countries.js
@@ -2,6 +2,6 @@ import apiClient from './axiosInterceptor';
 
 export async function getCountries() {
   return apiClient.get(
-    `https://restcountries.eu/rest/v2/all?fields=name;callingCodes`
+    `https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;callingCodes;flag`
   );
 }
