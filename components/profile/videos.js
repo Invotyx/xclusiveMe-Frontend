d6ccdf71d2b8f3fc029e043de5731b1a12bb0667
Salal Aslam <salalaslam@yahoo.com>
2021-06-02 09:17:19 +0500

    Update videos.js

--- components/profile/videos.js
+++ components/profile/videos.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import CardMedia from '@material-ui/core/CardMedia';
 import Card from '@material-ui/core/Card';
 import PostMediaVideo from './post-media-video';
 
