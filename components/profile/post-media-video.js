8304e98a3176f2ed72e7206a4449c6150dcf33ee
Salal Aslam <salalaslam@yahoo.com>
2021-06-02 09:09:56 +0500

    Update post-media-video.js

--- components/profile/post-media-video.js
+++ components/profile/post-media-video.js
@@ -1,37 +1,13 @@
-import { useEffect, useRef } from 'react';
-import Hls from 'hls.js';
+import ReactHlsPlayer from 'react-hls-player';
 
 export default function PostMediaVideo({ src }) {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    let hls;
-    if (videoRef.current) {
-      const video = videoRef.current;
-
-      if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        // Some browers (safari and ie edge) support HLS natively
-        video.src = src;
-      } else if (Hls.isSupported()) {
-        // This will run in all other modern browsers
-        hls = new Hls();
-        try {
-          hls.loadSource(src);
-          hls.attachMedia(video);
-        } catch (e) {
-          console.log('Failed to load media', e);
-        }
-      } else {
-        console.error("This is a legacy browser that doesn't support MSE");
-      }
-    }
-
-    return () => {
-      if (hls) {
-        hls.destroy();
-      }
-    };
-  }, [videoRef]);
-
-  return <video controls ref={videoRef} style={{ width: '100%' }} />;
+  return (
+    <ReactHlsPlayer
+      src={src}
+      autoPlay={false}
+      controls={true}
+      width='100%'
+      height='auto'
+    />
+  );
 }
