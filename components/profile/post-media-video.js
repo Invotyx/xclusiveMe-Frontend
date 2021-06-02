f3e32dea9d9bd986e0531039672107987306bc0f
Salal Aslam <salalaslam@yahoo.com>
2021-06-02 09:25:23 +0500

    Revert "Update post-media-video.js"
    This reverts commit 8304e98a3176f2ed72e7206a4449c6150dcf33ee.

--- components/profile/post-media-video.js
+++ components/profile/post-media-video.js
@@ -1,13 +1,37 @@
-import ReactHlsPlayer from 'react-hls-player';
+import { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
 
 export default function PostMediaVideo({ src }) {
-  return (
-    <ReactHlsPlayer
-      src={src}
-      autoPlay={false}
-      controls={true}
-      width='100%'
-      height='auto'
-    />
-  );
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    let hls;
+    if (videoRef.current) {
+      const video = videoRef.current;
+
+      if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        // Some browers (safari and ie edge) support HLS natively
+        video.src = src;
+      } else if (Hls.isSupported()) {
+        // This will run in all other modern browsers
+        hls = new Hls();
+        try {
+          hls.loadSource(src);
+          hls.attachMedia(video);
+        } catch (e) {
+          console.log('Failed to load media', e);
+        }
+      } else {
+        console.error("This is a legacy browser that doesn't support MSE");
+      }
+    }
+
+    return () => {
+      if (hls) {
+        hls.destroy();
+      }
+    };
+  }, [videoRef]);
+
+  return <video controls ref={videoRef} style={{ width: '100%' }} />;
 }
