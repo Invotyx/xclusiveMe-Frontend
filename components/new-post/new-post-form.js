3443568f116225d674535860fbe4df4a7f4ca61e
Salal Aslam <salalaslam@yahoo.com>
2021-06-03 11:32:30 +0500

    Revert "Update new-post-form.js"
    This reverts commit 6259004e600fc2d62599695ed8be0c5c0813c37c.

--- components/new-post/new-post-form.js
+++ components/new-post/new-post-form.js
@@ -65,16 +65,8 @@ export default function NewPostForm({ afterSave }) {
   const [loading, set_Loading] = React.useState(false);
   const [progressVideo, setProgressVideo] = React.useState({ val: 0 });
   const [loadingItems, setLoadingItems] = React.useState([]);
-  const [validationErrors, setValidationErrors] = React.useState(null);
 
   const handleCreatePost = () => {
-    setValidationErrors(null);
-    if (!postText || postText.trim() === '') {
-      setValidationErrors({
-        postText: { length: 'Post text cannot be empty' },
-      });
-      return;
-    }
     dispatch(
       post.save({
         saveData: {
@@ -294,7 +286,7 @@ export default function NewPostForm({ afterSave }) {
           variant='contained'
           size='large'
           startIcon={<SendIcon />}
-          disabled={disabled || validationErrors}
+          disabled={disabled}
         >
           Post now
         </GreenButton>
