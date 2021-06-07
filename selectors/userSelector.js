fdb232e0fd8ef7db737db55f2de47375c0073d30
Salal Aslam <salalaslam@yahoo.com>
2021-06-07 11:12:18 +0500

    Update userSelector.js

--- selectors/userSelector.js
+++ selectors/userSelector.js
@@ -1,22 +1,20 @@
 import { createSelector } from 'reselect';
 
-const userState = (state) => state.get('userData');
+const userState = state => state.get('userData');
 
-const userDataSelector = createSelector(userState, (state) => {
+const userDataSelector = createSelector(userState, state => {
   const data = state.get('data');
 
   return data;
 });
 
-const singleSelector = createSelector(userState, (state) =>
-  state.get('single')
-);
+const singleSelector = createSelector(userState, state => state.get('single'));
 
-const fetchingSelector = createSelector(userState, (state) =>
+const fetchingSelector = createSelector(userState, state =>
   state.get('fetching')
 );
 
-const errorSelector = createSelector(userState, (state) => {
+const errorSelector = createSelector(userState, state => {
   const error = state.get('error');
 
   return error;
