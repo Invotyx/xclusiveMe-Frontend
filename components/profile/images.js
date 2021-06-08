4698cf16573958eabbf53e2f11c1f9d1565f1a3e
honeynaik <51706603+hannannaik@users.noreply.github.com>
2021-06-08 09:45:42 +0500

    Update images.js

--- components/profile/images.js
+++ components/profile/images.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CardMedia from '@material-ui/core/CardMedia';
 import Card from '@material-ui/core/Card';
+import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   media: {
@@ -16,7 +17,11 @@ export default function ImageGridList({ imagesData }) {
 
   return (
     <Grid container spacing={2}>
-      {(!imagesData || imagesData.length === 0) && <div>no content</div>}
+      {(!imagesData || imagesData.length === 0) && (
+        <Box flex={1} textAlign='center' p={4}>
+          <div>no content</div>
+        </Box>
+      )}
       {imagesData?.map(
         (tile, i) =>
           tile && (
