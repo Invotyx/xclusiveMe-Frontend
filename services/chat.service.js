0304706f7b7caa42aaf45ca8f6913170346150aa
Zeeshan Haider Shaheen <85152075+zeeshaheen@users.noreply.github.com>
2021-08-11 16:05:27 +0500

    chat service(API)

---
+++ services/chat.service.js
@@ -0,0 +1,25 @@
+import apiClient from './axiosInterceptor';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+const SERVER_ADDRESS = publicRuntimeConfig.backendUrl;
+
+export async function send(saveData) {
+  const data = JSON.stringify(saveData);
+  return apiClient.post(`${SERVER_ADDRESS}/conversations`, data);
+}
+
+export async function getConversations(pageNum, limit) {
+  return apiClient.get(
+    `${SERVER_ADDRESS}/conversations?page=${pageNum}&limit=${limit}`
+  );
+}
+
+export async function getSingleChat(id, pageNum, limit) {
+  return apiClient.get(
+    `${SERVER_ADDRESS}/conversations/${id}/messages?page=${pageNum}&limit=${limit}`
+  );
+}
+
+export async function isSeen(id) {
+  return apiClient.patch(`${SERVER_ADDRESS}/conversations/${id}/messages/seen`);
+}
