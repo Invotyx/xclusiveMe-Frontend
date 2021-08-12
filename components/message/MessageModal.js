4ca9acbde21814dd9f4e5f9ba34724f8af79c424
Zeeshan Haider Shaheen <85152075+zeeshaheen@users.noreply.github.com>
2021-08-12 16:25:30 +0500

    chat initiate modal

---
+++ components/message/MessageModal.js
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import CloseIcon from '@material-ui/icons/Close';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+import ShareIcon from '@material-ui/icons/Share';
+import TextField from '@material-ui/core/TextField';
+import LoadingOverlay from 'react-loading-overlay';
+import BounceLoader from 'react-spinners/BounceLoader';
+import { chat as chatData } from '../../actions/chat';
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: 'black',
+    border: 'none',
+    boxShadow: theme.shadows[5],
+    width: 'auto',
+    height: 'auto',
+    borderRadius: '8px',
+  },
+}));
+
+const MessageModal = ({ messageModal, setMessageModal, profileData }) => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [purchased, setPurchased] = useState(false);
+  const isMobile = useMediaQuery({ query: '(max-width: 760px)' });
+  const [postText, set_postText] = useState('');
+  const pageNum = 1;
+  const limit = 10;
+
+  const handleSendMessage = () => {
+    // setPurchased(true);
+    if (!postText || postText.trim() === '') {
+      return;
+    }
+
+    dispatch(
+      chatData?.sendMessage({
+        saveData: {
+          content: postText,
+          sentTo: profileData?.id,
+          type: 'text',
+          isPaid: false,
+        },
+        callback: () => {
+          setMessageModal(false);
+          dispatch(
+            chatData?.getConversations({
+              pageNum: pageNum,
+              limit: limit,
+            })
+          );
+        },
+      })
+    );
+  };
+
+  const handleClose = () => {
+    setMessageModal(false);
+    setPurchased(false);
+  };
+
+  //   useEffect(() => {
+  //     dispatch(paymentMethod.request());
+  //   }, []);
+  return (
+    <Modal
+      aria-labelledby='transition-modal-title'
+      aria-describedby='transition-modal-description'
+      className={classes.modal}
+      open={messageModal}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={messageModal}>
+        <div className={classes.paper}>
+          <div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  width: '90%',
+                }}
+              >
+                <img
+                  src={profileData?.profileImage}
+                  alt='profile image'
+                  width='60px'
+                  height='65px'
+                  style={{
+                    borderRadius: '50%',
+                    marginTop: '-20px',
+                    marginLeft: isMobile ? '40%' : '44%',
+                  }}
+                />
+                <CloseIcon
+                  onClick={handleClose}
+                  style={{
+                    marginTop: '20px',
+                    width: '30px',
+                    height: '30px',
+                    cursor: 'pointer',
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <p style={{ fontWeight: '600', fontSize: '17px' }}>
+                  {/* Donate to {post?.user?.fullName} */}
+                  Send a message
+                </p>
+              </div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: '10px',
+                marginLeft: '10px',
+                marginRight: '10px',
+              }}
+            >
+              <div>
+                {/* <p
+                    style={{
+                      fontSize: '40px',
+                      fontWeight: '600',
+                    }}
+                  >
+                    ${post?.price}.00
+                  </p> */}
+
+                <TextField
+                  id='outlined-basic'
+                  variant='outlined'
+                  fullWidth
+                  multiline
+                  value={postText}
+                  onChange={e => set_postText(e.target.value)}
+                  rows={3}
+                  placeholder='Write something...'
+                  style={{ width: isMobile ? '80vw' : '30vw' }}
+                />
+              </div>
+            </div>
+
+            <Button
+              variant='contained'
+              style={{
+                backgroundColor: '#67E697',
+                color: 'white',
+                width: isMobile ? '80vw' : '30vw',
+                margin: '20px',
+                marginTop: '10px',
+              }}
+              onClick={handleSendMessage}
+            >
+              SEND NOW
+            </Button>
+          </div>
+        </div>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default MessageModal;
