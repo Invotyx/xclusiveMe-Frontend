1d11b412ae262fe413365bb2a196428cfc5a9717
Zeeshan Haider Shaheen <85152075+zeeshaheen@users.noreply.github.com>
2021-08-17 15:20:28 +0500

    user report redux

--- reducers/postReducer.js
+++ reducers/postReducer.js
@@ -37,6 +37,7 @@ export default function postReducer(state = initialState, action) {
     case POST.GET_SETTING_NOTIFICATIONS:
     case POST.ADD_SETTING_NOTIFICATIONS:
     case POST.REPORT_POST:
+    case POST.REPORT_USER:
     case POST.TIP:
       return state.merge(action.payload);
     default:
