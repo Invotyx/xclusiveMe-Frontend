f69f99c9b788eac2b85a5a7980e42a9631f648e8
Salal Aslam <salalaslam@yahoo.com>
2021-08-17 09:10:51 +0500

    Update Message.js

--- components/message/Message.js
+++ components/message/Message.js
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import MuiListItem from '@material-ui/core/ListItem';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import ImageAvatar from '../image-avatar';
 import { useMediaQuery } from 'react-responsive';
