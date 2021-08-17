65d49c0a5aa6069c98352fa632b8aaf6b9ff8c3c
Salal Aslam <salalaslam@yahoo.com>
2021-08-17 15:25:56 +0500

    Update TipModal.js

--- components/profile/TipModal.js
+++ components/profile/TipModal.js
@@ -77,78 +77,78 @@ const TipModal = ({ post }) => {
       <IconButton onClick={handleOpenTopModal}>
         <MonetizationOnOutlinedIcon />
       </IconButton>
-    <Modal
-      aria-labelledby='transition-modal-title'
-      aria-describedby='transition-modal-description'
-      className={classes.modal}
-      open={openTip}
-      onClose={handleClose}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{
-        timeout: 500,
-      }}
-    >
-      <Fade in={openTip}>
-        <div className={classes.paper}>
-          {purchased === false ? (
-            <div>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '90%',
-                  }}
-                >
-                  <img
-                    src={post?.user?.profileImage}
-                    alt='profile image'
-                    width='60px'
-                    height='65px'
-                    style={{
-                      borderRadius: '50%',
-                      marginTop: '-20px',
-                      marginLeft: isMobile ? '40%' : '44%',
-                    }}
-                  />
-                  <CloseIcon
-                    onClick={handleClose}
-                    style={{
-                      marginTop: '20px',
-                      width: '35px',
-                      height: '35px',
-                      cursor: 'pointer',
-                    }}
-                  />
-                </div>
-              </div>
+      <Modal
+        aria-labelledby='transition-modal-title'
+        aria-describedby='transition-modal-description'
+        className={classes.modal}
+        open={openTip}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openTip}>
+          <div className={classes.paper}>
+            {purchased === false ? (
               <div>
                 <div
                   style={{
                     display: 'flex',
                     justifyContent: 'center',
-                    alignItems: 'center',
                   }}
                 >
-                  <p style={{ fontWeight: '600', fontSize: '17px' }}>
-                    Donate to {post?.user?.fullName}
-                  </p>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      width: '90%',
+                    }}
+                  >
+                    <img
+                      src={post?.user?.profileImage}
+                      alt='profile image'
+                      width='60px'
+                      height='65px'
+                      style={{
+                        borderRadius: '50%',
+                        marginTop: '-20px',
+                        marginLeft: isMobile ? '40%' : '44%',
+                      }}
+                    />
+                    <CloseIcon
+                      onClick={handleClose}
+                      style={{
+                        marginTop: '20px',
+                        width: '35px',
+                        height: '35px',
+                        cursor: 'pointer',
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
                 <div>
-                  {/* <p
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <p style={{ fontWeight: '600', fontSize: '17px' }}>
+                      Donate to {post?.user?.fullName}
+                    </p>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <div>
+                    {/* <p
                     style={{
                       fontSize: '40px',
                       fontWeight: '600',
@@ -157,193 +157,193 @@ const TipModal = ({ post }) => {
                     ${post?.price}.00
                   </p> */}
 
-                  <div style={{ display: 'flex' }}>
-                    {/* <AttachMoneyIcon
+                    <div style={{ display: 'flex' }}>
+                      {/* <AttachMoneyIcon
                       style={{ fontSize: '60px', marginLeft: '-30px' }}
                     /> */}
 
-                    <form>
-                      <TextField
-                        id='outlined-basic'
-                        variant='outlined'
-                        placeholder='Enter amount in USD'
-                        value={addPrice}
-                        onChange={e => setAddPrice(e.target.value)}
-                        name='addPrice'
-                      />
-                    </form>
-                  </div>
-
-                  <p
-                    style={{
-                      color: '#444444',
-
-                      marginLeft: '65px',
-                    }}
-                  >
-                    Total Amount
-                  </p>
-                </div>
-              </div>
-
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-              >
-                <img src='/border.png' alt='border' width='90%' />
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'space-around',
-                  flexWrap: 'wrap',
-                }}
-              >
-                <p>Your default payment method</p>
-                <div>
-                  <div style={{ display: 'flex', marginLeft: '70px' }}>
-                    <p>{paymentData[0]?.type}</p>
-                  </div>
-                  <p
-                    style={{
-                      margin: '0px',
-                      marginLeft: '-30px',
-                      display: 'flex',
-                    }}
-                  >
-                    **** **** ****{' '}
-                    <p style={{ marginLeft: '10px', marginTop: '-4px' }}>
-                      {paymentData[0]?.last4_card}
-                    </p>
-                  </p>
-                </div>
-              </div>
-              <Button
-                variant='contained'
-                style={{
-                  backgroundColor: '#67E697',
-                  color: 'white',
-                  width: '92%',
-                  margin: '20px',
-                  marginTop: '10px',
-                }}
-                onClick={handlePurchase}
-              >
-                SEND NOW
-              </Button>
-            </div>
-          ) : (
-            <div
-              style={{
-                width: 'auto',
-                backgroundColor: '#000000',
-                borderRadius: '8px',
-              }}
-            >
-              <div
-                style={{
-                  backgroundColor: '#67E697',
-                  borderTopLeftRadius: '8px',
-                  borderTopRightRadius: '8px',
-                  height: '13vh',
-                }}
-              >
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-evenly',
-                    marginLeft: isMobile ? '15vw' : '8vw',
-                  }}
-                >
-                  <div>
-                    <p
-                      style={{
-                        fontSize: '24px',
-                        fontWeight: '600',
-                        marginTop: isMobile ? '35px' : '',
-                      }}
-                    >
-                      Sent Successfully
-                    </p>
-                  </div>
-                  <CloseIcon
-                    onClick={handleClose}
-                    style={{
-                      marginTop: isMobile ? '35px' : '25px',
-                      marginLeft: isMobile ? '40px' : '20px',
-                      marginRight: isMobile ? '20px' : '',
-                      width: '27px',
-                      height: '27px',
-                    }}
-                  />
-                </div>
-              </div>
-              <div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <img
-                    src={post?.user?.profileImage}
-                    alt='profile image'
-                    width='70px'
-                    height='75px'
-                    style={{
-                      borderRadius: '50%',
-                      marginTop: '20px',
-                    }}
-                  />
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    marginTop: '20px',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <p
-                    style={{
-                      fontSize: '26px',
-                      fontWeight: 'bold',
-                      marginTop: '-1vh',
-                    }}
-                  >
-                    {addPrice}.00
-                  </p>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-
-                    justifyContent: 'center',
-                  }}
-                >
-                  <p
-                    style={{
-                      marginTop: isMobile ? '-3vh' : '-4vh',
-                      color: '#444444',
-                    }}
-                  >
-                    sent to {post?.user?.fullName}
-                  </p>
-                </div>
-
-                {isMobile ? (
-                  <div>
-                    <div>
-                      <Button
-                        variant='outlined'
-                        onClick={handleClose}
-                        style={{
-                          margin: '20px',
-                          width: '90%',
-                          padding: '10px',
-                        }}
-                      >
-                        <CloseIcon className={styles.buttonIcons} /> Close
-                      </Button>
+                      <form>
+                        <TextField
+                          id='outlined-basic'
+                          variant='outlined'
+                          placeholder='Enter amount in USD'
+                          value={addPrice}
+                          onChange={e => setAddPrice(e.target.value)}
+                          name='addPrice'
+                        />
+                      </form>
                     </div>
 
-                    {/* <div>
+                    <p
+                      style={{
+                        color: '#444444',
+
+                        marginLeft: '65px',
+                      }}
+                    >
+                      Total Amount
+                    </p>
+                  </div>
+                </div>
+
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <img src='/border.png' alt='border' width='90%' />
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    flexWrap: 'wrap',
+                  }}
+                >
+                  <p>Your default payment method</p>
+                  <div>
+                    <div style={{ display: 'flex', marginLeft: '70px' }}>
+                      <p>{paymentData[0]?.type}</p>
+                    </div>
+                    <p
+                      style={{
+                        margin: '0px',
+                        marginLeft: '-30px',
+                        display: 'flex',
+                      }}
+                    >
+                      **** **** ****{' '}
+                      <p style={{ marginLeft: '10px', marginTop: '-4px' }}>
+                        {paymentData[0]?.last4_card}
+                      </p>
+                    </p>
+                  </div>
+                </div>
+                <Button
+                  variant='contained'
+                  style={{
+                    backgroundColor: '#67E697',
+                    color: 'white',
+                    width: '92%',
+                    margin: '20px',
+                    marginTop: '10px',
+                  }}
+                  onClick={handlePurchase}
+                >
+                  SEND NOW
+                </Button>
+              </div>
+            ) : (
+              <div
+                style={{
+                  width: 'auto',
+                  backgroundColor: '#000000',
+                  borderRadius: '8px',
+                }}
+              >
+                <div
+                  style={{
+                    backgroundColor: '#67E697',
+                    borderTopLeftRadius: '8px',
+                    borderTopRightRadius: '8px',
+                    height: '13vh',
+                  }}
+                >
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-evenly',
+                      marginLeft: isMobile ? '15vw' : '8vw',
+                    }}
+                  >
+                    <div>
+                      <p
+                        style={{
+                          fontSize: '24px',
+                          fontWeight: '600',
+                          marginTop: isMobile ? '35px' : '',
+                        }}
+                      >
+                        Sent Successfully
+                      </p>
+                    </div>
+                    <CloseIcon
+                      onClick={handleClose}
+                      style={{
+                        marginTop: isMobile ? '35px' : '25px',
+                        marginLeft: isMobile ? '40px' : '20px',
+                        marginRight: isMobile ? '20px' : '',
+                        width: '27px',
+                        height: '27px',
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <img
+                      src={post?.user?.profileImage}
+                      alt='profile image'
+                      width='70px'
+                      height='75px'
+                      style={{
+                        borderRadius: '50%',
+                        marginTop: '20px',
+                      }}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      marginTop: '20px',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <p
+                      style={{
+                        fontSize: '26px',
+                        fontWeight: 'bold',
+                        marginTop: '-1vh',
+                      }}
+                    >
+                      {addPrice}.00
+                    </p>
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <p
+                      style={{
+                        marginTop: isMobile ? '-3vh' : '-4vh',
+                        color: '#444444',
+                      }}
+                    >
+                      sent to {post?.user?.fullName}
+                    </p>
+                  </div>
+
+                  {isMobile ? (
+                    <div>
+                      <div>
+                        <Button
+                          variant='outlined'
+                          onClick={handleClose}
+                          style={{
+                            margin: '20px',
+                            width: '90%',
+                            padding: '10px',
+                          }}
+                        >
+                          <CloseIcon className={styles.buttonIcons} /> Close
+                        </Button>
+                      </div>
+
+                      {/* <div>
                       <Button
                         variant='outlined'
                         onClick={handleClose}
@@ -355,25 +355,25 @@ const TipModal = ({ post }) => {
                         <ShareIcon className={styles.buttonIcons} /> Share
                       </Button>
                     </div> */}
-                  </div>
-                ) : (
-                  <div style={{ display: 'flex' }}>
-                    <Button
-                      variant='outlined'
-                      onClick={handleClose}
-                      style={{
-                        margin: 'auto',
-                        width: '30vw',
-                        marginBottom: '20px',
-                        marginLeft: '10px',
-                        marginRight: '10px',
-                        padding: '10px',
-                      }}
-                    >
-                      <CloseIcon className={styles.buttonIcons} /> Close
-                    </Button>
+                    </div>
+                  ) : (
+                    <div style={{ display: 'flex' }}>
+                      <Button
+                        variant='outlined'
+                        onClick={handleClose}
+                        style={{
+                          margin: 'auto',
+                          width: '30vw',
+                          marginBottom: '20px',
+                          marginLeft: '10px',
+                          marginRight: '10px',
+                          padding: '10px',
+                        }}
+                      >
+                        <CloseIcon className={styles.buttonIcons} /> Close
+                      </Button>
 
-                    {/* <Button
+                      {/* <Button
                       variant='outlined'
                       onClick={handleClose}
                       style={{
@@ -383,14 +383,14 @@ const TipModal = ({ post }) => {
                     >
                       <ShareIcon className={styles.buttonIcons} /> Share
                     </Button> */}
-                  </div>
-                )}
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-      </Fade>
-    </Modal>
+            )}
+          </div>
+        </Fade>
+      </Modal>
     </>
   );
 };
