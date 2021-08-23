eed043deee2fd956c97af3263c7a4ff7a60f4d8b
Salal Aslam <salalaslam@yahoo.com>
2021-08-23 09:13:33 +0500

    Update ReportModal.js

--- components/profile/ReportModal.js
+++ components/profile/ReportModal.js
@@ -5,7 +5,7 @@ import Fade from '@material-ui/core/Fade';
 import CloseIcon from '@material-ui/icons/Close';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 import TextField from '@material-ui/core/TextField';
 import LoadingOverlay from 'react-loading-overlay';
@@ -35,7 +35,6 @@ const ReportModal = ({
   onConfirm,
 }) => {
   const classes = useStyles();
-  const dispatch = useDispatch();
   const isMobile = useMediaQuery({ query: '(max-width: 760px)' });
   const [postText, set_postText] = useState('');
   const fetchData = useSelector(fetchingSelector);
