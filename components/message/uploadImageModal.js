0dbeb680052d53dba37ff072e5de67a5b75d48eb
Salal Aslam <salalaslam@yahoo.com>
2021-08-25 14:12:47 +0500

    Update uploadImageModal.js

--- components/message/uploadImageModal.js
+++ components/message/uploadImageModal.js
@@ -214,7 +214,6 @@ export default function UploadImageModal({ conId }) {
         />
       </div>
 
-      <UploadImageModal />
       <GreenButton
         variant='contained'
         color='primary'
