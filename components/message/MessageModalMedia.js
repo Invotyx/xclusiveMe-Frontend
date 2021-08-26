d4c1fc456f06582b9b8fff4e3a080ce826fcd79f
Salal Aslam <salalaslam@yahoo.com>
2021-08-26 14:05:30 +0500

    Update MessageModalMedia.js

--- components/message/MessageModalMedia.js
+++ components/message/MessageModalMedia.js
@@ -125,7 +125,9 @@ export default function MessageModalMedia({ type, onMediaUploaded, children }) {
         )}
         {(type === 'photo' || type === 'video') && (
           <Box display='flex'>
+            <Box display='flex' flexGrow={1}>
             <Typography>Add media</Typography>
+            </Box>
             {type === 'photo' && (
               <>
                 <ImageList rowHeight={100} cols={4}>
