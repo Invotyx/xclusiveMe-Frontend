e9d7af2bbe489c363f25be0f284a0eafe7bdfd38
Salal Aslam <salalaslam@yahoo.com>
2021-08-26 10:33:10 +0500

    Update uploadVideo.js

--- components/new-post/uploadVideo.js
+++ components/new-post/uploadVideo.js
@@ -1,85 +1,17 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { post } from '../../actions/post';
 import IconButton from '@material-ui/core/IconButton';
 import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
-import * as UpChunk from '@mux/upchunk';
 import Box from '@material-ui/core/Box';
+import UploadVideoX from '../UploadVideo';
 
-export default function UploadVideo({
-  onUploadVideo,
-  onVideoSelect,
-  onVideoUploaded,
-  onVideoUploadProgress,
-  onVideoError,
-}) {
-  const dispatch = useDispatch();
-  const inputFile = React.useRef(null);
-
-  const onChangeFile = event => {
-    event.stopPropagation();
-    event.preventDefault();
-    var video = event.target.files[0];
-    if (video) {
-      onVideoSelect && onVideoSelect();
-
-      dispatch(
-        post.uploadVideoReq({
-          fileObject: 1,
-          callback: res => {
-            const upload = UpChunk.createUpload({
-              // getUploadUrl is a function that resolves with the upload URL generated
-              // on the server-side
-              endpoint: res.url,
-              // picker here is a file picker HTML element
-              file: video,
-              chunkSize: 5120, // Uploads the file in ~5mb chunks
-            });
-
-            // subscribe to events
-            upload.on('error', err => {
-              console.error('', err.detail);
-              onVideoError(err);
-            });
-
-            upload.on('progress', progress => {
-              console.log('Uploaded', progress.detail, 'percent of this file.');
-              onVideoUploadProgress(progress.detail);
-            });
-
-            // subscribe to events
-            upload.on('success', err => {
-              onUploadVideo(res.id, video.type);
-              onVideoUploaded && onVideoUploaded();
-              event.target.value = null;
-              console.log("Wrap it up, we're done here.");
-            });
-          },
-        })
-      );
-    }
-  };
-
+export default function UploadVideo(props) {
   return (
-    <>
-      <input
-        accept='video/*'
-        type='file'
-        ref={inputFile}
-        style={{ display: 'none' }}
-        onChange={onChangeFile}
-      />
-
+    <UploadVideoX {...props}>
       <Box clone color='#666'>
-        <IconButton
-          size='small'
-          onClick={() => {
-            inputFile.current.click();
-          }}
-        >
+        <IconButton size='small'>
           <VideocamOutlinedIcon />
         </IconButton>
       </Box>
-    </>
+    </UploadVideoX>
   );
 }
