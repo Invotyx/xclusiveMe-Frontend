13b17752ae80ec91a1cb4d60d0eeb3212f325ebe
Salal Aslam <salalaslam@yahoo.com>
2021-08-26 08:27:50 +0500

    Revert "Update uploadImageModal.js"
    This reverts commit 695b31037eab2988b5f9383740f46c3157061860.

--- components/message/uploadImageModal.js
+++ components/message/uploadImageModal.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
