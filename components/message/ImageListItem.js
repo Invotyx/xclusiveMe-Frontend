70b6f2354bca911dc023b587df220cf74e6c64d3
Salal Aslam <salalaslam@yahoo.com>
2021-08-27 15:56:00 +0500

    Update ImageListItem.js

--- components/message/ImageListItem.js
+++ components/message/ImageListItem.js
@@ -8,6 +8,14 @@ const useStyles = makeStyles(theme => ({
     width: theme.spacing(12),
     height: theme.spacing(12),
   },
+  children: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#00000080',
+  },
 }));
 
 export default function ImageListItem({ src, children, ...rest }) {
@@ -16,16 +24,7 @@ export default function ImageListItem({ src, children, ...rest }) {
   return (
     <Box position='relative' marginRight={1} {...rest}>
       <Avatar variant='square' src={src} className={classes.avatar} />
-      <Box
-        position='absolute'
-        top={0}
-        bottom={0}
-        left={0}
-        right={0}
-        bgcolor='#0000008'
-      >
-        {children}
-      </Box>
+      <Box className={classes.children}>{children}</Box>
     </Box>
   );
 }
