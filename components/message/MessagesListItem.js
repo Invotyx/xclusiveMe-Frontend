fb60058497af5618c770403eea93aac35602d3b7
Salal Aslam <salalaslam@yahoo.com>
2021-08-28 15:17:22 +0500

    MessagesListItem

---
+++ components/message/MessagesListItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { currentUserSelector } from '../../selectors/authSelector';
+import styles from './message.module.css';
+
+const MessagesListItem = ({ i, ...props }) => {
+  const current = useSelector(currentUserSelector);
+  const dispatch = useDispatch();
+
+  return (
+    <div
+      className={styles.container}
+      {...props}
+    >
+      <div className={styles.chatMessages}>
+        {i.sender.id !== current?.id ? (
+          <div className={styles.leftSide}>
+            <span className={styles.leftMessage}>{i.content}</span>
+            <div>{i.mediaLink && i.mediaLink}</div>
+          </div>
+        ) : (
+          <div className={styles.rightSide}>
+            <span className={styles.rightMessage}>{i.content}</span>
+            {i.mediaLink && (
+              <div
+                style={{
+                  marginTop: '10px',
+                  border: '1px solid #222222',
+                  borderRadius: '8px',
+                }}
+              >
+                <img
+                  src={i.mediaLink}
+                  alt=''
+                  style={{
+                    width: '200px',
+                    height: '200px',
+                    padding: '20px',
+                    borderRadius: '8px',
+                  }}
+                />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MessagesListItem;
