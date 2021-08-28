bceabbcbaeea9f1b5b5a0021c256da36ee67727f
Salal Aslam <salalaslam@yahoo.com>
2021-08-28 09:22:55 +0500

    Update MessageModalMediaFollowingsSelect.js

--- components/message/MessageModalMediaFollowingsSelect.js
+++ components/message/MessageModalMediaFollowingsSelect.js
@@ -1,8 +1,17 @@
 import React, { useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from '../../actions/auth';
+import { currentUserFollowingsSelector } from '../../selectors/authSelector';
 
 export default function MessageModalMediaFollowingsSelect(props) {
+  const followings = useSelector(currentUserFollowingsSelector);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(auth.requestFollowings());
+  }, []);
+
   return (
     <TextField
       select
@@ -13,6 +22,11 @@ export default function MessageModalMediaFollowingsSelect(props) {
       {...props}
     >
       <MenuItem>Select</MenuItem>
+      {followings?.results.map((f, i) => (
+        <MenuItem value={f.id} key={`followings${i}`}>
+          {f.user?.fullName || 'name'}
+        </MenuItem>
+      ))}
     </TextField>
   );
 }
