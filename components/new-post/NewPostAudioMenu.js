2fe7fa71d40158c1b71a69d42a41b4f9e5b67e2f
Salal Aslam <salalaslam@yahoo.com>
2021-08-31 11:12:32 +0500

    NewPostAudioMenu

---
+++ components/new-post/NewPostAudioMenu.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import GraphicEqRoundedIcon from '@material-ui/icons/GraphicEqRounded';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function NewPostAudioMenu({ startRecordingHandler }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButton size='small' style={{ color: '#666' }} onClick={handleClick}>
+        <GraphicEqRoundedIcon />
+      </IconButton>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={() => {
+            startRecordingHandler && startRecordingHandler();
+            handleClose();
+          }}
+        >
+          Record
+        </MenuItem>
+        <MenuItem onClick={handleClose}>Upload</MenuItem>
+      </Menu>
+    </>
+  );
+}
--- components/new-post/new-post-form.js
+++ components/new-post/new-post-form.js
@@ -14,7 +14,6 @@ import MuiImageListItemBar from '@material-ui/core/ImageListItemBar';
 import TextField from '@material-ui/core/TextField';
 import MuiOutlinedInput from '@material-ui/core/OutlinedInput';
 import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined';
-import GraphicEqRoundedIcon from '@material-ui/icons/GraphicEqRounded';
 import LocalOfferOutlinedIcon from '@material-ui/icons/LocalOfferOutlined';
 import AddIcon from '@material-ui/icons/Add';
 import { useDispatch } from 'react-redux';
@@ -29,6 +28,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import MessageModalMediaCamera from '../message/MessageModalMediaCamera';
 import useAudioSend from '../message/useAudioSend';
 import CheckIcon from '@material-ui/icons/Check';
+import NewPostAudioMenu from './NewPostAudioMenu';
 
 const useStyles = makeStyles(theme => ({
   alertIcon: {
@@ -295,11 +295,9 @@ export default function NewPostForm({ afterSave }) {
                 </Box>
               </Box>
               <Box mx={1}>
-                <Box clone color='#666'>
-                  <IconButton size='small' onClick={startRecordingHandler}>
-                    <GraphicEqRoundedIcon />
-                  </IconButton>
-                </Box>
+                <NewPostAudioMenu
+                  startRecordingHandler={startRecordingHandler}
+                />
               </Box>
               <Box mx={1}>
                 {_show_price_input ? (
