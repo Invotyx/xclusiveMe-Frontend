c4e713e68d3b5b9bbb2d8fc549542ce029f09d48
Zeeshan Haider Shaheen <85152075+zeeshaheen@users.noreply.github.com>
2021-08-31 15:37:09 +0500

    post report component

---
+++ components/profile/PostReport.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Card from '@material-ui/core/Card';
+import { CardContent, CardHeader } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 450,
+    backgroundColor: 'black',
+  },
+
+  icon: {
+    borderRadius: '50%',
+    width: 16,
+    height: 16,
+    boxShadow:
+      'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundColor: '#f5f8fa',
+    backgroundImage:
+      'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    '$root.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#ebf1f5',
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      background: 'rgba(206,217,224,.5)',
+    },
+  },
+  checkedIcon: {
+    backgroundColor: '#137cbd',
+    backgroundImage:
+      'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    '&:before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+      content: '""',
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
+    },
+  },
+});
+
+const reasons = [
+  {
+    text: 'I dont like this post',
+  },
+  {
+    text: 'The content is offensive or voilates "OnlyFans" Terms of Service',
+  },
+  {
+    text: 'This content contains stolen materal (DMCA)',
+  },
+  {
+    text: 'This content is spam',
+  },
+  {
+    text: 'Report Abuse',
+  },
+];
+
+// Inspired by blueprintjs
+function StyledRadio(props) {
+  const classes = useStyles();
+
+  return (
+    <Radio
+      disableRipple
+      color='default'
+      checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+      icon={<span className={classes.icon} />}
+      {...props}
+    />
+  );
+}
+
+export default function PostReport() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <FormControl component='fieldset'>
+          {reasons.map(r => (
+            <RadioGroup name='customized-radios'>
+              <FormControlLabel
+                value='female'
+                control={<StyledRadio />}
+                label='Female'
+              />
+            </RadioGroup>
+          ))}
+        </FormControl>
+      </CardContent>
+    </Card>
+  );
+}
