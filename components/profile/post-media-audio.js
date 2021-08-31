abce471e3f8c3c3e04c24ac5771de0b9b9373a8e
Salal Aslam <salalaslam@yahoo.com>
2021-08-31 08:30:43 +0500

    Update post-media-audio.js

--- components/profile/post-media-audio.js
+++ components/profile/post-media-audio.js
@@ -2,5 +2,10 @@ import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
 export default function PostMediaAudio(props) {
-  return <AudioPlayer {...props} onPlay={e => console.log('onPlay')} />;
+  return (
+    <AudioPlayer
+      {...props}
+      onPlay={e => console.log('onPlay')}
+    />
+  );
 }
