8d0d21405b50d0773b8baa107d5008f34160595f
Salal Aslam <salalaslam@yahoo.com>
2021-08-31 14:47:51 +0500

    Update chat.service.js

--- services/chat.service.js
+++ services/chat.service.js
@@ -36,8 +36,12 @@ export async function isSeen(id) {
 
 export async function uploadAudio(files) {
   const data = new FormData();
+  if (files.length) {
     for (let i = 0; i < files.length; i++) {
       data.append('audio', files[i]);
     }
+  } else {
+    data.append('audio', files);
+  }
   return apiClient.post(`${SERVER_ADDRESS}/uploads/audio`, data);
 }
