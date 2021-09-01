0c06fe35578807b961d9638fcd4a9821f6af0a8e
Salal Aslam <salalaslam@yahoo.com>
2021-09-01 11:58:08 +0500

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -79,6 +79,7 @@ export default function Home() {
                     startAdornment={<SearchIcon />}
                     value={_search}
                     onChange={e => {
+                      doSearch(e.target.value);
                       set_search(e.target.value);
                     }}
                   />
