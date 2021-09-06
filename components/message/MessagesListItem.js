b9dc4ed683a044b848feddf8b87b0bc1529525ec
Salal Aslam <salalaslam@yahoo.com>
2021-09-06 11:53:25 +0500

    Update MessagesListItem.js

--- components/message/MessagesListItem.js
+++ components/message/MessagesListItem.js
@@ -18,9 +18,7 @@ const MessagesListItem = ({
   ...props
 }) => {
   const { PostPurchaseModel, handleOpenModel } = usePostPurchaseModel();
-  const { ref, inView } = useInView({
-    threshold: 0,
-  });
+  const { ref, inView } = useInView({ threshold: 0 });
   const current = useSelector(currentUserSelector);
   const dispatch = useDispatch();
 
