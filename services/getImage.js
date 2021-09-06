141bd206dd378147d5e833280587d9d63eac61bc
Salal Aslam <salalaslam@yahoo.com>
2021-09-06 14:38:49 +0500

    Update getImage.js

--- services/getImage.js
+++ services/getImage.js
@@ -3,6 +3,6 @@ const { publicRuntimeConfig } = getConfig();
 const SERVER_ADDRESS = publicRuntimeConfig.backendUrl;
 
 export const getImage = img =>
-  img && img.indexOf('http') === -1
+  typeof img === 'string' && img.indexOf('http') === -1
     ? `${SERVER_ADDRESS.substring(0, SERVER_ADDRESS.length - 4)}/${img}`
     : img;
