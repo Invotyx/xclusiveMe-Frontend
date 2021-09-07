942f89adf782c391ca93b7199d273570050abbb4
Salal Aslam <salalaslam@yahoo.com>
2021-09-07 10:55:18 +0500

    suggestions

---
+++ components/Suggestions.js
@@ -0,0 +1,62 @@
+import NextLink from 'next/link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Link from '@material-ui/core/Link';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { user } from '../actions/user';
+import { allUsersSelector } from '../selectors/userSelector';
+import ImageAvatar from '../components/image-avatar';
+
+export default function Home() {
+  const dispatch = useDispatch();
+  const suggestion = useSelector(allUsersSelector);
+  const pageNum = Math.floor(Math.random() * 3 + 1);
+  const limit = Math.floor(Math.random() * 5 + 1);
+
+  useEffect(() => {
+    dispatch(user.requestAll({ limit: limit, pageNumber: pageNum }));
+  }, []);
+
+  return (
+    <>
+      <Typography>Suggestions For You</Typography>
+      <List>
+        {suggestion?.map(
+          (s, i) =>
+            s?.username && (
+              <ListItem key={`suggestions${i}`}>
+                <ListItemAvatar>
+                  <ImageAvatar src={s?.profileImage} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Typography variant='body2'>{s.fullName}</Typography>
+                  }
+                  secondary={
+                    <Typography variant='caption' color='textSecondary'>
+                      Suggested for you
+                    </Typography>
+                  }
+                />
+
+                <ListItemSecondaryAction>
+                  <NextLink href={`/x/${s.username}`} passHref>
+                    <Link>
+                      <div>
+                        <Typography variant='caption'>See Profile</Typography>
+                      </div>
+                    </Link>
+                  </NextLink>
+                </ListItemSecondaryAction>
+              </ListItem>
+            )
+        )}
+      </List>
+    </>
+  );
+}
--- pages/explore.js
+++ pages/explore.js
@@ -1,43 +1,23 @@
-import NextLink from 'next/link';
 import { motion } from 'framer-motion';
 import { variants } from '../services/framer-variants';
 import Head from 'next/head';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Link from '@material-ui/core/Link';
+import Suggestions from '../components/Suggestions';
 import Post from '../components/profile/post';
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../components/layouts/layout-auth';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { post } from '../actions/post';
 import { subscribedSelector } from '../selectors/postSelector';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import NothingHere from '../components/profile/nothing-here';
 import { useMediaQuery } from 'react-responsive';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { fetchingSelector } from '../selectors/postSelector';
-import { chat } from '../actions/chat';
-import { user } from '../actions/user';
-import { allUsersSelector } from '../selectors/userSelector';
-import ImageAvatar from '../components/image-avatar';
 
-const suggestions = [
-  {
-    profileImage: 'https://material-ui.com/static/images/avatar/1.jpg',
-    fullName: 'John Doe',
-    username: 'johndoe',
-  },
-];
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   root: {
     marginBottom: '150px',
   },
@@ -49,13 +29,6 @@ export default function Home() {
   const posts = useSelector(subscribedSelector);
   const isMobile = useMediaQuery({ query: '(max-width: 760px)' });
   const fetching = useSelector(fetchingSelector);
-  const suggestion = useSelector(allUsersSelector);
-  const pageNum = Math.floor(Math.random() * 3 + 1);
-  const limit = Math.floor(Math.random() * 5 + 1);
-
-  useEffect(() => {
-    dispatch(user.requestAll({ limit: limit, pageNumber: pageNum }));
-  }, []);
 
   useEffect(() => {
     dispatch(post.requestSubscribed());
@@ -108,46 +81,7 @@ export default function Home() {
                   paddingLeft: '16px',
                 }}
               >
-                <Typography>Suggestions For You</Typography>
-                <List>
-                  {suggestion?.map(
-                    (s, i) =>
-                      s?.username && (
-                        <ListItem key={`suggestions${i}`}>
-                          <ListItemAvatar>
-                            <ImageAvatar src={s?.profileImage} />
-                          </ListItemAvatar>
-                          <ListItemText
-                            primary={
-                              <Typography variant='body2'>
-                                {s.fullName}
-                              </Typography>
-                            }
-                            secondary={
-                              <Typography
-                                variant='caption'
-                                color='textSecondary'
-                              >
-                                Suggested for you
-                              </Typography>
-                            }
-                          />
-
-                          <ListItemSecondaryAction>
-                            <NextLink href={`/x/${s.username}`} passHref>
-                              <Link>
-                                <div>
-                                  <Typography variant='caption'>
-                                    See Profile
-                                  </Typography>
-                                </div>
-                              </Link>
-                            </NextLink>
-                          </ListItemSecondaryAction>
-                        </ListItem>
-                      )
-                  )}
-                </List>
+                <Suggestions />
               </Grid>
             )}
           </Grid>
