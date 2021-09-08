fe0d909831fad735502ccbd272cd4c79d07a2bd4
Salal Aslam <salalaslam@yahoo.com>
2021-09-08 13:08:49 +0500

    Update SetupAccountDialog.js

--- components/layouts/SetupAccountDialog.js
+++ components/layouts/SetupAccountDialog.js
@@ -1,7 +1,6 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
@@ -176,13 +175,6 @@ export default function SetupAccountDialog({ buttonProps, ...props }) {
             )}
           </Box>
         </DialogContent>
-        <DialogActions>
-          {activeStep > 0 && (
-            <Button onClick={() => setActiveStep(s => s - 1)} color='primary'>
-              Back
-            </Button>
-          )}
-        </DialogActions>
       </Dialog>
     </>
   );
