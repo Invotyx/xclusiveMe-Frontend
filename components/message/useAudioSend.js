5f377c1ba670d19395edb973b833bb546900d5e3
Salal Aslam <salalaslam@yahoo.com>
2021-09-09 14:54:45 +0500

    Delete audioSend.js

--- components/message/useAudioSend.js
+++ components/message/useAudioSend.js
@@ -2,7 +2,15 @@ import useMediaRecorder from '@wmik/use-media-recorder';
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { snackbar } from '../../actions/snackbar';
-import AudioSend from './audioSend';
+import {
+  IconButton,
+  LinearProgress,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+import moment from 'moment';
+import ClearIcon from '@material-ui/icons/Clear';
+import { chat } from '../../actions/chat';
 
 export default function useAudioSend() {
   let { status, mediaBlob, stopRecording, startRecording } = useMediaRecorder({
@@ -60,7 +68,7 @@ export default function useAudioSend() {
 
   return {
     AudioSend: props => (
-      <AudioSend
+      <AudioSendComp
         stopRecording={stopRecording}
         mediaBlob={mediaBlob}
         progress={progress}
@@ -74,4 +82,88 @@ export default function useAudioSend() {
     startRecordingHandler,
     isRecording,
   };
+}
+
+function AudioSendComp({
+  stopRecording,
+  mediaBlob,
+  progress,
+  seconds,
+  progressRef,
+  setProgress,
+  setIsRecording,
+  onAudioUploaded,
+  finishIcon,
+}) {
+  const dispatch = useDispatch();
+
+  const voiceSendHandler = () => {
+    stopRecording();
+    setTimeout(() => onStop(mediaBlob), 500);
+  };
+
+  const Clear = () => {
+    clearInterval(progressRef);
+    setProgress(0);
+    setIsRecording(false);
+    stopRecording();
+  };
+
+  const onStop = audioData => {
+    const date = moment();
+    date.format('YYYY-MM-DD');
+
+    const audioFile = new File([audioData], `abc.wav`, {
+      type: 'audio/wav',
+      lastModified: date,
+    });
+    voiceMailSend([audioFile]);
+  };
+
+  const voiceMailSend = audioFile => {
+    clearInterval(progressRef);
+    dispatch(
+      chat.sendVoicemail({
+        audioFile,
+        callback: data => {
+          onAudioUploaded(data);
+          Clear();
+        },
+      })
+    );
+  };
+
+  const formatTime = () => {
+    const getSeconds = `0${seconds % 60}`.slice(-2);
+    const minutes = `${Math.floor(seconds / 60)}`;
+    const getMinutes = `${minutes % 60}`.slice(-2);
+    // const getHours = `0${Math.floor(seconds / 3600)}`.slice(-2)
+
+    return `${getMinutes} : ${getSeconds}`;
+  };
+
+  return (
+    <Paper
+      elevation={10}
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton onClick={Clear}>
+        <ClearIcon />
+      </IconButton>
+      <LinearProgress
+        style={{ display: 'flex', width: '70%' }}
+        variant='determinate'
+        value={progress}
+      />
+      <Typography style={{ display: 'flex' }}>{formatTime()}</Typography>
+      <IconButton onClickCapture={voiceSendHandler}>
+        {finishIcon || <img src='/send.png' alt='send button' />}
+      </IconButton>
+    </Paper>
+  );
 }
