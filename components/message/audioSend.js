4dabb0ae0be33145fd0c3c41f19c5af9824964bd
Salal Aslam <salalaslam@yahoo.com>
2021-09-09 14:49:48 +0500

    Update audioSend.js

--- components/message/audioSend.js
+++ components/message/audioSend.js
@@ -24,6 +24,7 @@ export default function audioSend({
   const dispatch = useDispatch();
 
   const voiceSendHandler = () => {
+    stopRecording();
     onStop(mediaBlob);
   };
 
@@ -47,7 +48,6 @@ export default function audioSend({
 
   const voiceMailSend = audioFile => {
     clearInterval(progressRef);
-    stopRecording();
     dispatch(
       chat.sendVoicemail({
         audioFile,
