b2d3175bd8ff438bc7c0ed47dc7588f8e844a37c
Salal Aslam <salalaslam@yahoo.com>
2021-09-30 08:48:58 +0500

    Update auth-nav.js

--- components/layouts/auth-nav.js
+++ components/layouts/auth-nav.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   grey: {
     color: '#666',
   },
