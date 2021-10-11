ddf70c43ef04aded1bf6d5920a6e3aad0bf184e0
Zeeshan Haider Shaheen <85152075+zeeshaheen@users.noreply.github.com>
2021-10-11 15:32:01 +0500

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -85,7 +85,7 @@ export default function Home() {
                     inputProps={{
                       style: {
                         fontFamily: 'Poppins',
-                        fontWeight: 'bold',
+
                         marginLeft: '5px',
                       },
                     }}
